9df62aec652d2eafffdb415d96f00a153332c8f5
jl777 <jl777@users.noreply.github.com>
2014-04-22 18:54:13 -0500

    Update libwebsocketsglue.h

--- libwebsockets/libwebsocketsglue.h
+++ libwebsockets/libwebsocketsglue.h
@@ -1,6 +1,6 @@
 //
 //  libwebsocketsglue.h
-//  Created by jl777, April  2014
+//  Created by jl777, April 5th, 2014
 //  MIT License
 //
 
@@ -11,7 +11,7 @@
 int URL_changed;
 char dispstr[65536];
 char testforms[1024*1024];
-char NXTPROTOCOL_HTMLFILE[512] = { "/Users/jl777/gateway/NXTprotocol.html" };
+char NXTPROTOCOL_HTMLFILE[512] = { "/tmp/NXTprotocol.html" };
 
 #include "NXTprotocol.c"
 
@@ -30,12 +30,13 @@ char *changeurl_func(char *sender,int32_t valid,cJSON **objs,int32_t numobjs)
     return(retstr);
 }
 
-#include "InstantDEX.h"
+//#include "InstantDEX.h"
 #include "multigateway.h"
 #include "NXTorrent.h"
 #include "NXTsubatomic.h"
-#include "nodecoin.h"
-#include "NXTmixer.h"
+
+//#include "nodecoin.h"
+//#include "NXTmixer.h"
 #include "html.h"
 
 #define STUB_SIG 0x99999999
@@ -57,7 +58,7 @@ char *stub_jsonhandler(cJSON *argjson)
 void process_stub_AM(struct stub_info *dp,struct NXT_protocol_parms *parms)
 {
     cJSON *argjson;
-    char *jsontxt;
+   // char *jsontxt;
     struct json_AM *ap;
     char NXTaddr[64],*sender,*receiver;
     sender = parms->sender; receiver = parms->receiver; ap = parms->AMptr; //txid = parms->txid;
@@ -67,18 +68,10 @@ void process_stub_AM(struct stub_info *dp,struct NXT_protocol_parms *parms)
         printf("unexpected NXTaddr %s != sender.%s when receiver.%s\n",NXTaddr,sender,receiver);
         return;
     }
-    if ( ap->jsonflag != 0 )
+    if ( (argjson = parse_json_AM(ap)) != 0 )
     {
-        jsontxt = (ap->jsonflag == 1) ? ap->jsonstr : decode_json(&ap->jsn);
-        if ( jsontxt != 0 )
-        {
-            printf("process_stub_AM got jsontxt.(%s)\n",jsontxt);
-            argjson = cJSON_Parse(jsontxt);
-            if ( argjson != 0 )
-            {
-                free_json(argjson);
-            }
-        }
+        printf("process_stub_AM got jsontxt.(%s)\n",ap->jsonstr);
+        free_json(argjson);
     }
 }
 
@@ -115,6 +108,63 @@ void *stub_handler(struct NXThandler_info *mp,struct NXT_protocol_parms *parms,v
     return(dp);
 }
 
+#include <errno.h>
+
+ssize_t Readline(int sockd, void *vptr, size_t maxlen) {
+    ssize_t n, rc;
+    char    c, *buffer;
+    
+    buffer = vptr;
+    
+    for ( n = 1; n < (int)maxlen; n++ ) {
+        
+        if ( (rc = read(sockd, &c, 1)) == 1 ) {
+            *buffer++ = c;
+            if ( c == '\n' )
+                break;
+        }
+        else if ( rc == 0 ) {
+            if ( n == 1 )
+                return 0;
+            else
+                break;
+        }
+        else {
+            if ( errno == EINTR )
+                continue;
+            return -1;
+        }
+    }
+    
+    *buffer = 0;
+    return n;
+}
+
+
+/*  Write a line to a socket  */
+
+ssize_t Writeline(int sockd, const void *vptr, size_t n) {
+    size_t      nleft;
+    ssize_t     nwritten;
+    const char *buffer;
+    
+    buffer = vptr;
+    nleft  = n;
+    
+    while ( nleft > 0 ) {
+        if ( (nwritten = write(sockd, buffer, nleft)) <= 0 ) {
+            if ( errno == EINTR )
+                nwritten = 0;
+            else
+                return -1;
+        }
+        nleft  -= nwritten;
+        buffer += nwritten;
+    }
+    
+    return n;
+}
+
 void init_NXTprotocol()
 {
     static char *whitelist[] = { NXTISSUERACCT, NXTACCTA, NXTACCTB, NXTACCTC, "" };
@@ -124,20 +174,21 @@ void init_NXTprotocol()
     init_NXTAPI();
     memset(mp,0,sizeof(*mp));
     safecopy(mp->ipaddr,get_ipaddr(),sizeof(mp->ipaddr));
-    //mp->upollseconds = 1000 * 1;
+    mp->upollseconds = 100000 * 0;
     mp->pollseconds = POLL_SECONDS;
     //set_passwords(mp,0,0);
     safecopy(mp->NXTAPISERVER,NXTSERVER,sizeof(mp->NXTAPISERVER));
-    
     gen_randomacct(33,mp->NXTADDR,mp->NXTACCTSECRET,"randvals");
-    printf(">>>>>>>>>>>>>>> %s NXT.(%s)\n",mp->ipaddr,mp->NXTADDR);
+  printf(">>>>>>>>>>>>>>> %s NXT.(%s)\n",mp->ipaddr,mp->NXTADDR);
     register_NXT_handler("NXTorrent",mp,NXTPROTOCOL_ILLEGALTYPE,NXTPROTOCOL_ILLEGALTYPE,NXTorrent_handler,NXTORRENT_SIG,1,0,0);
-    //register_NXT_handler("multigateway",mp,2,-1,multigateway_handler,GATEWAY_SIG,1,0,whitelist);
+    SUPPRESS_MULTIGATEWAY = 1;
+    register_NXT_handler("subatomic",mp,NXTPROTOCOL_ILLEGALTYPE,NXTPROTOCOL_ILLEGALTYPE,subatomic_handler,SUBATOMIC_SIG,1,0,0);
+    register_NXT_handler("multigateway",mp,2,-1,multigateway_handler,GATEWAY_SIG,1,0,whitelist);
     //register_NXT_handler("InstantDEX",mp,NXTPROTOCOL_ILLEGALTYPE,NXTPROTOCOL_ILLEGALTYPE,iDEXhandler,INSTANTDEX_SIG,1,0,0);
-    //register_NXT_handler("subatomic",mp,NXTPROTOCOL_ILLEGALTYPE,NXTPROTOCOL_ILLEGALTYPE,subatomic_handler,SUBATOMIC_SIG,1,0,0);
     //register_NXT_handler("stub",mp,-1,-1,stub_handler,STUB_SIG,1,0,0);//whitelist);
     mp->firsttimestamp = issue_getTime();
-    start_NXTloops(mp,"2408234822244879292");//"4475075929652596255");//"14006431524478461177");//"12980781150532356708");
+    
+start_NXTloops(mp,"16787696303645624065");//"2408234822244879292");//"4475075929652596255");//"14006431524478461177");//"12980781150532356708");
 }
 
 
