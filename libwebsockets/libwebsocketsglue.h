d13da780965dc9ebe5b8afd840b95f1a65e2485b
jl777 <jl777@users.noreply.github.com>
2014-05-05 20:38:11 -0500

    Update libwebsocketsglue.h

--- libwebsockets/libwebsocketsglue.h
+++ libwebsockets/libwebsocketsglue.h
@@ -30,8 +30,8 @@ char *changeurl_func(char *sender,int32_t valid,cJSON **objs,int32_t numobjs)
     return(retstr);
 }
 
-#include "InstantDEX.h"
-#include "multigateway.h"
+#include "InstantDEX/InstantDEX.h"
+#include "multigateway/multigateway.h"
 #include "NXTorrent.h"
 #include "NXTsubatomic.h"
 
@@ -108,80 +108,75 @@ void *stub_handler(struct NXThandler_info *mp,struct NXT_protocol_parms *parms,v
     return(dp);
 }
 
-#include <errno.h>
-
-ssize_t Readline(int sockd, void *vptr, size_t maxlen) {
-    ssize_t n, rc;
-    char    c, *buffer;
-    
-    buffer = vptr;
-    
-    for ( n = 1; n < (int)maxlen; n++ ) {
-        
-        if ( (rc = read(sockd, &c, 1)) == 1 ) {
-            *buffer++ = c;
-            if ( c == '\n' )
-                break;
-        }
-        else if ( rc == 0 ) {
-            if ( n == 1 )
-                return 0;
-            else
-                break;
-        }
-        else {
-            if ( errno == EINTR )
-                continue;
-            return -1;
-        }
-    }
-    
-    *buffer = 0;
-    return n;
-}
-
-
-/*  Write a line to a socket  */
-
-ssize_t Writeline(int sockd, const void *vptr, size_t n) {
-    size_t      nleft;
-    ssize_t     nwritten;
-    const char *buffer;
-    
-    buffer = vptr;
-    nleft  = n;
-    
-    while ( nleft > 0 ) {
-        if ( (nwritten = write(sockd, buffer, nleft)) <= 0 ) {
-            if ( errno == EINTR )
-                nwritten = 0;
-            else
-                return -1;
-        }
-        nleft  -= nwritten;
-        buffer += nwritten;
-    }
-    
-    return n;
-}
-
-void init_NXTprotocol()
+void init_NXTprotocol(int _argc,char **_argv)
 {
+    static char *argv[16]; int argc = 0;
     static char *whitelist[] = { NXTISSUERACCT, NXTACCTA, NXTACCTB, NXTACCTC, "" };
-
+    
     struct NXThandler_info *mp = calloc(1,sizeof(*mp));    // seems safest place to have main data structure
     Global_mp = mp;
+    
     strcpy(MY_IPADDR,get_ipaddr());
     init_NXTAPI();
     memset(mp,0,sizeof(*mp));
 
     safecopy(mp->ipaddr,MY_IPADDR,sizeof(mp->ipaddr));
-    mp->upollseconds = 100000 * 0;
+    mp->upollseconds = 333333 * 0;
     mp->pollseconds = POLL_SECONDS;
-    //set_passwords(mp,0,0);
     safecopy(mp->NXTAPISERVER,NXTSERVER,sizeof(mp->NXTAPISERVER));
     gen_randomacct(33,mp->NXTADDR,mp->NXTACCTSECRET,"randvals");
-    mp->accountjson = issue_getAccountInfo(&Global_mp->acctbalance,mp->dispname,PC_USERNAME,mp->NXTADDR);
+    crypto_box_keypair(Global_mp->session_pubkey,Global_mp->session_privkey);
+    init_hexbytes(Global_mp->pubkeystr,Global_mp->session_pubkey,sizeof(Global_mp->session_pubkey));
+    mp->accountjson = issue_getAccountInfo(&Global_mp->acctbalance,mp->dispname,PC_USERNAME,mp->NXTADDR,mp->groupname);
+    printf("argv.0 %s, argv.1 %s (%s) (%s) (%s) (%s) [%s]\n",_argv[0],_argv[1],mp->dispname,PC_USERNAME,mp->NXTADDR,mp->groupname,mp->NXTACCTSECRET);
+ 
+    //printf("%s\n",issue_signTransaction("0000be25c6009a0225c5fed2690701cf06f267e7c227b1a3c0dfa9c6fc3cdb593b3af6f16d65302f9b30f378f284e10500e1f5050000000000e1f505000000000000000000000000000000000000000000000000000000000000000000000000")); getchar();
+
+    mp->myind = -1;
+    mp->nxt64bits = calc_nxt64bits(mp->NXTADDR);
+    argv[argc++] = "punch";
+    stripwhite(mp->dispname,strlen(mp->dispname));
+    argv[argc++] = "-u", argv[argc++] = mp->dispname;
+    argv[argc++] = "-n", argv[argc++] = mp->NXTADDR;
+    if ( mp->groupname[0] != 0 )
+        argv[argc++] = "-g", argv[argc++] = mp->groupname;
+    init_NXThashtables(mp);
+    if ( pthread_create(malloc(sizeof(pthread_t)),NULL,process_hashtablequeues,mp) != 0 )
+        printf("ERROR hist process_hashtablequeues\n");
+    if ( _argc < 2 || strcmp(_argv[1],"punch") != 0 )
+    {
+        char cmd[1024]; 
+        if ( _argc >= 2 && (strcmp(_argv[1],"server") == 0 || strcmp(mp->ipaddr,EMERGENCY_PUNCH_SERVER) == 0) )
+        {
+#ifndef WIN32
+           punch_server_main(_argc-1,_argv+1);
+            printf("hole punch server done\n"); exit(0);
+#endif
+        }
+        else if ( 0 )
+        {
+            launch_app_in_new_terminal(_argv[0],argc,argv);
+            sprintf(cmd,"say -v Vicki hello %s %s",mp->dispname,mp->groupname[0]==0?"please input groupname":"");
+#ifdef __APPLE__
+            //system(cmd);
+#endif
+        }
+    }
+    else if ( 0 )
+    {
+#ifndef WIN32
+        if ( pthread_create(malloc(sizeof(pthread_t)),NULL,punch_server_glue,argv) != 0 )
+            printf("ERROR punch_server_glue\n");
+        punch_client_main(argc,argv);
+        printf("chat child done\n"); while(1) sleep(1);
+#endif
+    }
+    if ( pthread_create(malloc(sizeof(pthread_t)),NULL,run_NXTsync,EMERGENCY_PUNCH_SERVER) != 0 )
+        printf("ERROR run_NXTsync\n");
+   // if ( pthread_create(malloc(sizeof(pthread_t)),NULL,process_syncmem_queue_loop,EMERGENCY_PUNCH_SERVER) != 0 )
+    //    printf("ERROR process_syncmem_queue_loop\n");
+   // sleep(60);
+    sleep(3);
     printf(">>>>>>>>>>>>>>> %s: %s %s NXT.(%s)\n",mp->dispname,PC_USERNAME,mp->ipaddr,mp->NXTADDR);
     register_NXT_handler("NXTorrent",mp,NXTPROTOCOL_ILLEGALTYPE,NXTPROTOCOL_ILLEGALTYPE,NXTorrent_handler,NXTORRENT_SIG,1,0,0);
     SUPPRESS_MULTIGATEWAY = 1;
@@ -191,7 +186,8 @@ void init_NXTprotocol()
     //register_NXT_handler("stub",mp,-1,-1,stub_handler,STUB_SIG,1,0,0);//whitelist);
     mp->firsttimestamp = issue_getTime();
     
-start_NXTloops(mp,"16787696303645624065");//"2408234822244879292");//"4475075929652596255");//"14006431524478461177");//"12980781150532356708");
+    //start_NXTloops(mp,"16787696303645624065"); has subatomic and atomic trades setup
+    start_NXTloops(mp,"1238805504845503558");
 }
 
 
